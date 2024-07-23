0a77659dfe0141ac7a32573bde229d422ea088a4
Benmeddour Samy <samy.benmeddour@gmail.com>
2024-07-23 06:00:30 +0200

    - Enable edge to edge on android sample

--- composeApp/src/androidMain/kotlin/fr/dcs/mdk/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/fr/dcs/mdk/MainActivity.kt
@@ -1,13 +1,14 @@
 package fr.dcs.mdk
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.*
 import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
     setContent { App() }
   }
 
